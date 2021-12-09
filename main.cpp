9ff7b9d61aa7addb377a92564d1340fa8d052e7a
Ahmed2122001 <93153148+Ahmed2122001@users.noreply.github.com>
2021-12-09 22:44:41 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,119 +1,197 @@
 #include <fstream>
 #include <string>
 #include <cstring>
+#include <algorithm>
 #include <iostream>
 using namespace std;
-class employee{
+class employee {
 public:
     char Employee_ID[13];
     char Employee_Dept_ID[13];
     char Employee_Name[50];
     char Employee_postion[50];
     fstream emp;
+    fstream primaryindex;
+    fstream secondry;
+
     string headerFile;
 public:
-    employee(){}
-    employee(const employee &obj){}
-    void changeheaderfile(int numrec,int RRn)
-    {
-        emp.open("employee.txt",ios::out);
-        emp<<numrec<<RRn;
+    employee() {}
 
-    }
-    void addemp(){
-        fstream emp ("employee.txt", ios::out);
-        employee e;
-        char more = 'n';
-        do{
+  //  employee(const employee &obj) {}
 
+//    void changeheaderfile(int numrec, int RRn) {
+//        emp.open("employee.txt", ios::out);
+//        emp << numrec << RRn;
+//
+//    }
 
-            cout << "\n employee  ID:\n";
-            cin.getline(e.Employee_ID,13);
-            emp<<e.Employee_ID<<',';
+//   void search(){
+//       binary_search(arr[])
+//   }
 
-            cout << "\n employee Department ID:\n";
-            cin.getline(e.Employee_Dept_ID,13);
-            emp<<e.Employee_Dept_ID<<',';
-
-            cout << "\n employee Name: \n";
-            cin.getline(e.Employee_Name, 50);
-            emp<<e.Employee_Name<<',';
-
-            cout << "\n employee position: \n";
-            cin.getline(e.Employee_postion,50);
-            emp<<e.Employee_postion<<'$';
-
-            cout << "Do you want to enter more employee?";
-            cin >> more;
-            cin.ignore();
-
-        } while (toupper(more) == 'Y');
-
-        emp.close();
-
-    }
-    employee GetEmployee(int RNN, fstream &infile) {
-        employee employee;
-        infile.seekg(RNN * 40, ios::beg);
-        infile.read((char*) &employee, sizeof employee);
-        return employee;
-    }
 };
+//    employee GetEmployee(int RNN, fstream &infile) {
+//        employee employee;
+//        infile.seekg(RNN * 40, ios::beg);
+//        infile.read((char*) &employee, sizeof employee);
+//        return employee;
+//    }
+//
+//    };
+struct PIndex
+{
+    int RRN;
+    string ID;
+};
+struct SIndex
+{
+    string ID_e;
+    string Id_D;
+};
+//void WritePrimIndex(PIndex PrmIndxArray[],int numRec,fstream&outfile) {
+//    for(int i =0;i<numRec;i++){
+//        outfile.write((char*)&PrmIndxArray[i].RRN,sizeof(int));
+//        outfile.write(PrmIndxArray[i].ID.c_str(),13);
+//    }
+//}
+void addemp(int num) {
+    fstream emp("employee.txt", ios::out);
+    employee e;
+    e.primaryindex.open("p_indx.txt", ios::out);
+    e.secondry.open("sec.txt",ios::out);
+    PIndex *p=new PIndex[num];
+    SIndex *s=new SIndex[num];
+
+    for(int i=0;i<num;i++){
+        cout << "\n employee  ID:\n";
+        cin.getline(e.Employee_ID, 13);
+        emp << e.Employee_ID << ',';
+        p[i].ID=e.Employee_ID;
+        p[i].RRN=i;
+        s[i].ID_e=e.Employee_ID;
+        e.primaryindex<<p[i].RRN<<",";
+        e.primaryindex << e.Employee_ID<<"$";
+        e.secondry<<e.Employee_ID<<",";
+
+
+
+        cout << "\n employee Department ID:\n";
+        cin.getline(e.Employee_Dept_ID, 13);
+        emp << e.Employee_Dept_ID << ',';
+        s[i].Id_D=e.Employee_Dept_ID;
+        e.secondry<<e.Employee_Dept_ID<<"$";
+
+        cout << "\n employee Name: \n";
+        cin.getline(e.Employee_Name, 50);
+        emp << e.Employee_Name << ',';
+
+        cout << "\n employee position: \n";
+        cin.getline(e.Employee_postion, 50);
+        emp << e.Employee_postion << '$';
+
+
+
+
+    }
+    emp.close();
+    e.primaryindex.close();
+    e.secondry.close();
+}
+
+//void ReadPrimIndex(PIndex PrmIndxArray[],int numRec,fstream& inFile) {
+//    for(int i =0;i<numRec;i++){
+//        inFile.read((char*)&PrmIndxArray[i].RRN,sizeof(int));
+//        char* tmp=new char[13];
+//        inFile.read(tmp,13);
+//        tmp[9]=0;
+//        PrmIndxArray[i].ID=tmp;
+//    }
+//void SearchById(int numRec) {
+//    fstream file, PrimIndex;
+//
+//    file.open("file1.txt", ios::in);
+//    PrimIndex.open("Primary.txt", ios::in);
+//
+//
+//    PIndex *PrmIndxArray = new PIndex[numRec];
+//    employee *emp = new employee[numRec];
+//
+//    ReadPrimIndex(PrmIndxArray, numRec, PrimIndex);
+//
+//
+//    //printing index files
+//
+//    cout << "Primary index" << endl;
+//    for (int i = 0; i < numRec; i++) {
+//        cout << "RRN : " << PrmIndxArray[i].RRN << "  ID:" << PrmIndxArray[i].ID << endl;
+//    }
+//}
 
 class department{
+public:
     char Dept_ID[30];
     char Dept_Name[50];
     char Dept_Manger[50];
     fstream dept;
-
-public:
-    void adddept(){
-        fstream dept("dept.txt", ios::out);
-        department d;
-        char more = 'n';
-        do{
-
-
-            cout << "\n department   ID:\n";
-            cin.getline(d.Dept_ID,30);
-            dept<<d.Dept_ID<<',';
-
-            cout << "\n  Department Name:\n";
-            cin.getline(d.Dept_Name,50);
-            dept<<d.Dept_Name<<',';
-
-            cout << "\n Department manger: \n";
-            cin.getline(d.Dept_Manger, 50);
-            dept<<d.Dept_Manger<<'$';
+    fstream d_index;
+    fstream s_index;
 
 
 
-            cout << "Do you want to enter more department?";
-            cin >> more;
-            cin.ignore();
-
-        } while (toupper(more) == 'Y');
-
-        dept.close();
-
-    }
 };
+struct dept_index
+{
+    int RRN;
+    string ID;
+};
+struct dsIndex
+{
+    string D_n;
+    string D_id;
+};
+void adddept(int num){
+    fstream dept("dept.txt", ios::out);
+    department d;
+    d.d_index.open("d_index",ios::out);
+    d.s_index.open("sec_dept.txt",ios::out);
+    dept_index *t=new dept_index[num];
+    dsIndex *s=new dsIndex[num];
+
+    for(int i=0;i<num;i++)
+    {
+
+        cout << "\n department   ID:\n";
+        cin.getline(d.Dept_ID,30);
+        dept<<d.Dept_ID<<',';
+        t[i].ID=d.Dept_ID;
+        t[i].RRN=i;
+        d.d_index<<t[i].RRN<<",";
+        d.d_index << d.Dept_ID<<"$";
+        s[i].D_id=d.Dept_ID;
+        d.s_index<<d.Dept_ID<<",";
+
+        cout << "\n  Department Name:\n";
+        cin.getline(d.Dept_Name,50);
+        dept<<d.Dept_Name<<',';
+        s[i].D_n=d.Dept_Name;
+        d.s_index<<d.Dept_Name<<"$";
+
+
+        cout << "\n Department manger: \n";
+        cin.getline(d.Dept_Manger, 50);
+        dept<<d.Dept_Manger<<'$';
+    }
+    dept.close();
+    d.d_index.close();
+    d.s_index.close();
+
+}
 int main() {
-   // employee e;
-  department d;
-   // e.addemp();
-   d.adddept();
+  //  employee e;
+  //department d;
+    addemp(1);
+   //adddept(1);
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
